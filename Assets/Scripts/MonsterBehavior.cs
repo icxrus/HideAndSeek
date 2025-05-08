02b7507d1da1502189750a65360d641614fc4542
icxrus <emppu.hietikko@gmail.com>
2025-05-08 19:04:00 +0300

    Script planning

--- Assets/Scripts/MonsterBehavior.cs
+++ Assets/Scripts/MonsterBehavior.cs
@@ -1,15 +1,35 @@
+using System.Threading;
+using UnityEditor.Searcher;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MonsterBehavior : MonoBehaviour
 {
+    //Monster components
     private NavMeshAgent agent;
+    private Animator animator;
+
+    //External data
     [SerializeField] private GameObject currentPathingTarget;
-    [SerializeField] private bool canSeePlayer = false;
     [SerializeField] private GameObject player;
+
+    //Variables
+    [SerializeField] private bool canSeePlayer = false;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
+
+   /* 
+    Monster behavior
+    1. Go to new room or stay
+	    - if player in room higher chance to stay
+    2. Search room
+        2b.check hiding spot or look around
+        2c. if player in check hiding spot -> roar and kill
+	        -> if not, go back to 1. 
+   */
+
 }
